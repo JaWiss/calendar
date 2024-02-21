2e70835ec3d1e2d989d639800617083029079d7f
jhws42 <jann.wissbach@mni.thm.de>
2024-02-21 18:31:51 +0100

    added function to create folder for json months file and to create the json files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
-use serde_json::{self, to_string_pretty};
+use std::fs::{self, create_dir, File};
 
 mod structs;
 use structs::day::Date;
 
 fn main() {
-    let tag = Date::new(20,"Februar".to_string(),2024, "Essen".to_string());
-    :
+    let tag = Date::new(29,"Februar".to_string(),2024, "Essen".to_string());
+    let folder_path = String::from("months");
+    let months: [&str; 12] = ["January", "February", "March", "April", "May", 
+    "June", "July", "August", "September", "October", "November", "December"];
+    if let Err(_) = fs::metadata(&folder_path) {
+        match fs::create_dir(&folder_path) {
+            Ok(_) => println!("Folder created."),
+            Err(e) => eprintln!("Error creating folder: {}", e),
+        }
+    } else {
+        println!("Folder already exists.");
+    }
+    for month in &months {
+        let mut file_name:String = String::new(); 
+        file_name.push_str(&folder_path);
+        file_name.push_str("/");
+        file_name.push_str(month);
+        file_name.push_str(".json");
+        create_month_file(&file_name);
+    }
+}
+
+fn create_month_file(month: &str) {
+   if let Err(_) = fs::metadata(&month) {
+       match File::create(&month) {
+           Ok(_) => println!("file created"),
+           Err(_) => println!("Error while creating file"),
+           
+       }
+   } else {
+       println!("file already exists");
+   }
+}
+
+fn save_date(date: &Date) {
+    
 }
